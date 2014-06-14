376cc76cbc7a03f4faf59e095b95648386464004
syntaxerror <opensource@syntaxerror.no>
2014-06-14 13:04:48 +0200

    Changing files charset and translating Norwegian characters into HTML character entities, in order to make it work for both Latin1 and UTF-8 configured stores, without needing to re-save the file with right charset.

--- src/DIBS Payment Window/includes/languages/norwegian/modules/payment/dibspw.php
+++ src/DIBS Payment Window/includes/languages/norwegian/modules/payment/dibspw.php
@@ -22,28 +22,28 @@
   define('MODULE_PAYMENT_DIBSPW_TEXT_EMAIL_FOOTER', 'DIBS transaksjonreferanse: ');
   define('MODULE_PAYMENT_DIBSPW_SETUP_ERROR_TITLE', 'Installasjonsfeil');
   define('MODULE_PAYMENT_DIBSPW_ERROR_TITLE', 'Betaling avbrutt');
-  define('MODULE_PAYMENT_DIBSPW_ERROR_DEFAULT', 'Din betaling ble ikke fullført. Vennligst forsøk igjen, eller velg annen betalingsmåte. Kontakt butikkeier dersom problemet vedvarer.');
+  define('MODULE_PAYMENT_DIBSPW_ERROR_DEFAULT', 'Din betaling ble ikke fullf&oslash;rt. Vennligst fors&oslash;k igjen, eller velg annen betalingsm&aring;te. Kontakt butikkeier dersom problemet vedvarer.');
   define('MODULE_PAYMENT_DIBSPW_TEXT_PROCESSING_PAYMENT', 'Behandler din betaling ...');
   define('MODULE_PAYMENT_DIBSPW_HEADING_TITLE', 'Nettbetaling');
   define('MODULE_PAYMENT_DIBSPW_STATUS_PAYMENT', 'Transaksjon');
   define('MODULE_PAYMENT_DIBSPW_MSG_TOSHOP', 'Returner til butikken');
-  define('MODULE_PAYMENT_DIBSPW_MSG_REDIR', 'Klikk på knappen nedenfor dersom du ikke ble videresent automatisk til butikken.');
+  define('MODULE_PAYMENT_DIBSPW_MSG_REDIR', 'Klikk p&aring; knappen nedenfor dersom du ikke ble videresent automatisk til butikken.');
   define('MODULE_PAYMENT_DIBSPW_MSG_REDIRTITLE', 'Videresender til butikk ...');
   define('MODULE_PAYMENT_DIBSPW_MSG_ERRCODE', 'Feilkode:');
   define('MODULE_PAYMENT_DIBSPW_MSG_ERRCODE', 'Feilmelding:');
-  define('MODULE_PAYMENT_DIBSPW_ERR_0', 'En feil har oppstått under verifisering av betaling.');
+  define('MODULE_PAYMENT_DIBSPW_ERR_0', 'En feil har oppst&aring;tt under verifisering av betaling.');
   define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_2', 'En ukjent ordre-id ble returnert fra DIBS.');
   define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_1', 'Ingen ordre-id ble returnert fra DIBS.');
-  define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_4', 'Beløp som ble returnert fra DIBS avviker fra det opprinnelige ordrebeløpet.');
-  define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_3', 'Ingen beløp ble returnert fra DIBS.');
+  define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_4', 'Bel&oslash;p som ble returnert fra DIBS avviker fra det opprinnelige ordrebel&oslash;pet.');
+  define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_3', 'Ingen bel&oslash;p ble returnert fra DIBS.');
   define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_6', 'Valuta som ble returnert fra DIBS avviker fra opprinnelig valuta for ordre.');
   define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_5', 'Ingen valuta ble returnert fra DIBS.');
-  define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_7', '\'Fingerprint\'-nøkkel stemmer ikke.');
+  define('MODULE_PAYMENT_DIBSPW_TEXT_ERR_7', '\'Fingerprint\'-n&oslash;kkel stemmer ikke.');
 
   // Only load overrides at checkout confirmation page when DIBS is selected, else it makes no sense.
   if ($_GET['main_page'] == 'checkout_confirmation') {
-    define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Bekreft din ordre for å gå videre til betaling');
-    define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', '- hvor du fullfører ordreprosessen.');
+    define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Bekreft din ordre for &aring; g&aring; videre til betaling');
+    define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', '- hvor du fullf&oslash;rer ordreprosessen.');
     /* Why are the following strings overridden? A change in those in the middle of the process is only confusing.
     define('NAVBAR_TITLE_1', 'Kasse');
     define('NAVBAR_TITLE_2', 'Transaksjon');
